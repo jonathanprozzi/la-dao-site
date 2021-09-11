0e17946ba661338bb95712e3b898648eff0888d0
jonathanprozzi <jonathanprozzi@gmail.com>
2021-09-10 23:22:45 -0400

    updated tsconfig to add components route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,15 @@
-import { Box, Heading, Text, Button } from "@chakra-ui/react";
+import { Box, Container, Stack, Heading, Text } from "@chakra-ui/react";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <Box>
-      <Heading>NextJS, Chakra, and Typescript!</Heading>
-      <Text>Some basic examples here.</Text>
-      <Button colorScheme='brandGreen'>
-        This button uses the custom colorScheme
-      </Button>
+    <Box minWidth='100vw' minHeight='100vh'>
+      <Hero />
+      {/* <AboutSection />
+      <WhatWeDoSection />
+      <ServicesSection />
+      <ProjectsSection />
+      <JoinSection /> */}
     </Box>
   );
 }
