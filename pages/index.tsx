58d3d851218b13c7f64476a8a713040b4fab6ddf
jonathanprozzi <jonathanprozzi@gmail.com>
2021-09-13 08:34:32 -0400

    removed extra gradient that was displaying odd on iOS

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,15 +9,9 @@ export default function Home() {
   return (
     <Box minWidth='100vw' minHeight='100vh'>
       <Hero />
-      <Box
-        width='100vw'
-        background='linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0) 0.01%, rgba(255, 255, 255, 0) 0.02%, #000000 100%)'
-        minHeight='10vh'
-      />
       <SubHero />
       <ServicesSection />
       <SocialSection />
-
       <FooterSection />
     </Box>
   );
